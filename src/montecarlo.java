4e73697428e5392be9a7b59be8aa12056d63cb74
zzehracamci-ux <zzehracamci@gmail.com>
2025-12-30 23:03:48 +0300

    montecarlo.java

--- src/montecarlo.java
+++ src/montecarlo.java
@@ -1,11 +1,11 @@
-package teamwork3first;
+package teamwork3;
 import java.util.Random;
 public class montecarlo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		        int n = 10000; // Önerilen gözlem sayısı
+		        int n = 10000; // recommended observation
 		        int pointsInsideCircle = 0;
 		        Random random = new Random();
 
@@ -13,19 +13,19 @@ public class montecarlo {
 		        System.out.println("--------------------");
 
 		        for (int i = 1; i <= n; i++) {
-		            // 1. [0,1] aralığında rastgele x ve y koordinatları üret
+		            
 		            double x = random.nextDouble();
 		            double y = random.nextDouble();
 
-		            // 2. x^2 + y^2 <= 1 koşulunu kontrol et
+		            // 2. x^2 + y^2 <= 1 
 		            if (Math.pow(x, 2) + Math.pow(y, 2) <= 1) {
 		                pointsInsideCircle++;
 		            }
 
-		            // 3. Pi tahminini hesapla: 4 * (daire içindeki / toplam)
+		            // 3. Pi estimation: 4 * (inside circle / sum)
 		            double piEstimate = 4.0 * pointsInsideCircle / i;
 
-		            // Belirli aralıklarla gelişimi konsola yazdır (Grafik verisi için)
+		           
 		            if (i % 1000 == 0 || i == 1) {
 		                System.out.printf("%d\t%.5f%n", i, piEstimate);
 		            }
@@ -39,5 +39,6 @@ public class montecarlo {
 		
 
 	}
+
 
 
