cac5a8b5d1271f04b4c05c037040b5e7bb16a998
zzehracamci-ux <zzehracamci@gmail.com>
2026-01-10 14:30:50 +0300

    Update Clt1.java

--- IE221-TeamWork-Group5/src/Clt1.java
+++ IE221-TeamWork-Group5/src/Clt1.java
@@ -12,7 +12,7 @@
 	 * Demonstrates how the sum of independent variables follows a 
 	 * Normal Distribution regardless of the original distribution. [cite: 23, 27]
 	 */
-	public class work1 {
+	public class CLT {
 
 	    public static void main(String[] args) {
 	        // Array of different sample sizes to show convergence rate [cite: 37, 41]
